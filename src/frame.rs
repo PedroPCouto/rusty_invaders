6e44064656d58856e836abce27c390b3c2c9ef2a
EloinxD <pedrohpaiva1258@gmail.com>
2022-11-11 22:14:32 -0300

    finishing project

--- src/frame.rs
+++ src/frame.rs
@@ -1,19 +1,11 @@
 use crate::{NUM_COLS, NUM_ROWS};
 
-pub type Frame = Vec<Vec<& 'static str>>;
+pub type Frame = [[char; NUM_ROWS]; NUM_COLS];
 
 pub fn new_frame() -> Frame {
-    let mut cols = Vec::with_capacity(NUM_COLS);
-    for _ in 0..NUM_COLS {
-        let mut col = Vec::with_capacity(NUM_ROWS);
-        for _ in 0..NUM_ROWS {
-            col.push(" ");
-        }
-        cols.push(col);
-    }
-    cols
+    [[' '; NUM_ROWS]; NUM_COLS]
 }
 
 pub trait Drawable {
-    fn draw(&self, fram: &mut Frame);
+    fn draw(&self, frame: &mut Frame);
 }
--- src/shot.rs
+++ src/shot.rs
@@ -1,25 +1,24 @@
-use crate::{NUM_COLS, NUM_ROWS};
-use rusty_time::timer:Timer;
-use std::time:Duration;
-use invader_project::frame;
+use crate::frame::{Drawable, Frame};
+use rusty_time::timer::Timer;
+use std::time::Duration;
 
 pub struct Shot {
     pub x: usize,
-    pub y: usize.
+    pub y: usize,
     pub exploding: bool,
-    timer: Timer
+    timer: Timer,
 }
 
 impl Shot {
-    pub fn new(x: usize, y:usize) -> Self {
+    pub fn new(x: usize, y: usize) -> Self {
         Self {
             x,
             y,
             exploding: false,
-            timer: Timer::from_millis(50)
+            timer: Timer::from_millis(50),
         }
     }
-    pub fn update(&mut self, delta: Duration){
+    pub fn update(&mut self, delta: Duration) {
         self.timer.update(delta);
         if self.timer.ready && !self.exploding {
             if self.y > 0 {
@@ -28,18 +27,17 @@ impl Shot {
             self.timer.reset();
         }
     }
-    pub fn explode(&mut self){
+    pub fn explode(&mut self) {
         self.exploding = true;
         self.timer = Timer::from_millis(250);
     }
     pub fn dead(&self) -> bool {
-        (self.exploding && self.timer.ready) || (self.y ==0)
+        (self.exploding && self.timer.ready) || (self.y == 0)
     }
 }
 
 impl Drawable for Shot {
-    fn draw(&self, &mut frame: Frame){
-        frame[self.x][self.y] = if self.exploding {"*"} else{"|"};
+    fn draw(&self, frame: &mut Frame) {
+        frame[self.x][self.y] = if self.exploding { '*' } else { '|' };
     }
 }
-
